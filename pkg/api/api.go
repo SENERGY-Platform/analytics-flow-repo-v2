b1ed33c27bf49da032573324917704caef4384ad
TZ <zsco@users.noreply.github.com>
2025-10-20 14:48:14 +0200

    sync version

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -33,7 +33,7 @@ import (
 
 // New godoc
 // @title Analytics-Flow-Repo-V2 API
-// @version 0.0.23
+// @version 0.0.25
 // @description For the administration of analytics flows.
 // @license.name Apache-2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
