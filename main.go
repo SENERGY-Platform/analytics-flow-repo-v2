e6ceaddd86fb4290ccec1ad3b92c054720b19c54
TZ <zsco@users.noreply.github.com>
2025-11-05 15:24:14 +0100

    add logging org

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ import (
 	permV2Client "github.com/SENERGY-Platform/permissions-v2/pkg/client"
 )
 
-var version = "0.0.26"
+var version = "0.0.27"
 
 func main() {
 	srvInfoHdl := srv_info_hdl.New("analytics-flow-repo-v2", version)
