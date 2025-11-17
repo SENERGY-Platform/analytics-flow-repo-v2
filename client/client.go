53d04f075b05d846b089046af7b2c5f57cc59450
franzmueller <franzmueller@users.noreply.github.com.>
2025-11-17 11:33:31 +0100

    add client

---
+++ client/client.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2025 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Client struct {
+	baseUrl string
+}
+
+func NewClient(baseUrl string) *Client {
+	return &Client{baseUrl: baseUrl}
+}
+
+func doNoDecode(req *http.Request, token string, userId string) (resp *http.Response, code int, err error) {
+	req.Header.Set("Authorization", token)
+	req.Header.Set("X-UserId", userId)
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return resp, http.StatusInternalServerError, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode > 299 {
+		temp, _ := io.ReadAll(resp.Body) //read error response end ensure that resp.Body is read to EOF
+		return resp, resp.StatusCode, fmt.Errorf("unexpected statuscode %v: %v", resp.StatusCode, string(temp))
+	}
+	return
+}
+
+func do[T any](req *http.Request, token string, userId string) (result T, code int, err error) {
+	resp, code, err := doNoDecode(req, token, userId)
+	if err != nil {
+		return result, code, err
+	}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		_, _ = io.ReadAll(resp.Body) //ensure resp.Body is read to EOF
+		return result, http.StatusInternalServerError, err
+	}
+	return
+}
---
+++ client/flows.go
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2025 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/SENERGY-Platform/analytics-flow-repo-v2/lib"
+)
+
+func (c *Client) GetFlows(token string, userId string) (resp lib.FlowsResponse, code int, err error) {
+	req, err := http.NewRequest(http.MethodGet, c.baseUrl+"/flow", nil)
+	if err != nil {
+		return resp, http.StatusBadRequest, err
+	}
+	return do[lib.FlowsResponse](req, token, userId)
+}
+
+func (c *Client) GetFlow(token string, userId string, id string) (flow lib.Flow, code int, err error) {
+	req, err := http.NewRequest(http.MethodGet, c.baseUrl+"/flow/"+id, nil)
+	if err != nil {
+		return flow, http.StatusBadRequest, err
+	}
+	return do[lib.Flow](req, token, userId)
+}
+
+func (c *Client) CreateFlow(token string, userId string, flow lib.Flow) (code int, err error) {
+	b, err := json.Marshal(flow)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	req, err := http.NewRequest(http.MethodPut, c.baseUrl+"/flow/", bytes.NewBuffer(b))
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	_, code, err = doNoDecode(req, token, userId)
+	return code, err
+}
+
+func (c *Client) UpdateFlow(token string, userId string, flow lib.Flow) (code int, err error) {
+	if flow.Id == nil {
+		return http.StatusBadRequest, fmt.Errorf("flow needs non-nil id")
+	}
+	b, err := json.Marshal(flow)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	req, err := http.NewRequest(http.MethodPost, c.baseUrl+"/flow/"+flow.Id.String(), bytes.NewBuffer(b))
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	_, code, err = doNoDecode(req, token, userId)
+	return code, err
+}
+
+func (c *Client) DeleteFlow(token string, userId string, id string) (code int, err error) {
+	req, err := http.NewRequest(http.MethodDelete, c.baseUrl+"/flow/"+id, nil)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	_, code, err = doNoDecode(req, token, userId)
+	return code, err
+}
